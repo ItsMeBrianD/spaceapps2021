9b59ae5d0f617bf73433acea16f14d7ef9bfd288
Brian <contact@noimbrian.com>
2021-10-02 18:02:28 -0500

    CHANGE PLACES

--- svelte/src/lib/components/atoms/Map.svelte
+++ svelte/src/lib/components/atoms/Map.svelte
@@ -1,93 +1,41 @@
 <script lang="ts">
 	import {onDestroy, onMount} from "svelte";
-	import {
-Map, TileLayer, ParticleLayer, VectorLayer, Circle, Marker, Point
-} from "maptalks";
+	// import * as worldwind from "@nasaworldwind/worldwind";
+
 	let el: HTMLElement | undefined;
-	let map;
-	onMount(() => {
-	    map = new Map(el, {
-     		center: [0, 0],
-			 draggable: false,
-			 dragPan: false,
-			 dragRotate: false,
-			 dragPitch: false,
-			 minZoom: 2.5,
-			 maxExtent: [0, 0, 180, 180],
-			 scrollWheelZoom: false,
-			 touchZoom: false,
-			 doubleClickZoom: false,
-     		zoom: 2,
-     		baseLayer: new TileLayer("base", {
-       			"urlTemplate" : "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
-       			"subdomains"  : ["a", 'b', 'c', 'd'],
-       			"attribution"  : "&copy; <a href=\"http://www.osm.org/copyright\">OSM</a> contributors, "+
-       			"&copy; <a href=\"https://carto.com/attributions\">CARTO</a>"
-     		}),
-			 layers : [
-         		 new VectorLayer('v')
-        	]
-	    });
-		
-	    window.map = map;
-	    const particles = new ParticleLayer("MYPARTICLELAYER");
-		const center = map.getCenter();
-		console.log(center);
+	let canvas: HTMLCanvasElement | undefined;
+	onMount(async () => {
+		const WorldWind = await import("@nasaworldwind/worldwind");
+		console.log(WorldWind);
+		WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING)
 
-	    particles.getParticles = function(t) {
-	        return [
-	            {
-					point: new Point({
-		                x: Math.sin(t) * 10,
-		                y: 5,
-	    	        }),
-	        	    r: 40,
-	            	color: "rgb(0,255,0)"
-				},
-				{
-					point: new Point({
-		                y: Math.cos(t) * 10,
-		                x: 5,
-	    	        }),
-	        	    r: 40,
-	            	color: "rgb(255,0,0)"
-				},
-	        ];
-	    };
-	    map.addLayer(particles);
-
-		new Marker(center, {
-        symbol : {
-          markerType : 'cross',
-          markerWidth : 10,
-          markerHeight : 10,
-          markerLineWidth : 2
-        }
-      })
-      .addTo(map.getLayer('v'));
-
-      new Circle(center, 1000, {
-        symbol : {
-          lineColor : '#fff',
-          lineWidth : 6,
-          lineOpacity : 0.2,
-          polygonOpacity : 0
-        }
-      })
-      .addTo(map.getLayer('v'));
-		
+		const worldWindow = new WorldWind.WorldWindow(canvas);
+		const layers = [
+            // Imagery layers.
+            {layer: new WorldWind.BMNGLayer(), enabled: true},
+            {layer: new WorldWind.DigitalGlobeTiledImageLayer("Digital Globe", "digitalglobe.n6ngnadl", accessToken),
+                enabled: true},
+            {layer: new WorldWind.DigitalGlobeTiledImageLayer("Digital Globe with Roads", "digitalglobe.n6nhclo2", accessToken),
+                enabled: false},
+            // Add atmosphere layer on top of all base layers.
+            {layer: new WorldWind.AtmosphereLayer(), enabled: true},
+            // WorldWind UI layers.
+            {layer: new WorldWind.CompassLayer(), enabled: true},
+            {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
+            {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
+        ];
 	});
 	onDestroy(() => {
-	    map.remove();
-	    window.map = undefined;
+	    
 	});
 
 </script>
 
+<canvas bind:this={canvas}/>
 <div bind:this={el}/>
 
 <style lang="postcss">
-	div {
+	div, canvas {
 		@apply h-full w-full;
 	}
 </style>
