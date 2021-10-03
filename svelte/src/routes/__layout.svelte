0afaac68545efd85df5b6a7b82a33093a1354821
Brian <contact@noimbrian.com>
2021-10-03 16:36:03 -0500

    Add Github links and stuff

--- svelte/src/routes/__layout.svelte
+++ svelte/src/routes/__layout.svelte
@@ -3,3 +3,6 @@
 </script>
 
 <slot />
+<a class="absolute bottom-6 right-6 w-8 z-50" target="_blank" href="https://github.com/0c370t/spaceapps2021">
+	<img class="m-0" src="/GitHub-Mark-Light-120px-plus.png" alt="Github Link"/>
+</a>
