ce4d5c6cb3d713c00cc5b699964adee737743f9d
Andreas <andreas.m.wenzel@gmail.com>
2021-10-03 10:24:57 -0700

    debuggand speed increases

--- svelte/src/lib/components/atoms/WasmDebug.svelte
+++ svelte/src/lib/components/atoms/WasmDebug.svelte
@@ -5,35 +5,35 @@
   const {wasmMod: wasm} = getContext(ContextKeys.WasmStore);
 
   async function render() {
-      const tles = await fetch("/norad/catalog.txt").then(async r => r.text());
+    const tles = await fetch("/norad/norad.txt").then(async r => r.text());
 
-      const enc = new TextEncoder();
-      const tleArray = enc.encode(tles);
-      const dataPtr = wasm._malloc(tleArray.length);
-      const dataHeap = new Uint8Array(wasm.HEAPU8.buffer, dataPtr, tleArray.length);
-      dataHeap.set(tleArray);
+    let enc = new TextEncoder();
+    let tleArray = enc.encode(tles);
+    let dataPtr = wasm._malloc(tleArray.length);
+    let dataHeap = new Uint8Array(wasm.HEAPU8.buffer, dataPtr, tleArray.length);
+    dataHeap.set(tleArray);
 
-      let start = new Date();
-      wasm.loadObjs(dataPtr, tleArray.length);
-      let end = new Date();
-      let elapsed = end.getTime() - start.getTime();
-      console.log(`LOAD: ${elapsed}ms`);
+    let start = new Date();
+    wasm.loadObjs(dataPtr, tleArray.length);
+    let end = new Date();
+    let elapsed = end.getTime() - start.getTime();
+    console.log(`LOAD: ${elapsed}ms`);
 
-      let retVector;
-      for (let i = 0;i < 10000;i++) {
-          start = new Date();
-          console.log(Math.floor(start.getTime() / 1000));
-          retVector = wasm.getPositions(Math.floor(start.getTime() / 1000));
-          end = new Date();
-          elapsed = end.getTime() - start.getTime();
-          console.log(`CALC: ${elapsed}ms to calculate ${retVector.size()} positions`);
-          retVector.delete();
-      }
-      for (let i = 0;i < 5;i++) {
-          const info = retVector.get(i);
-          console.log(`${info.get(0)}\t${info.get(1)}\t${info.get(2)}\t${info.get(3)}`);
-      }
-
+    let retVector;
+    for (let i = 0; i < 100; i++) {
+      const secPerMonth = 30*24 *60 *60;
+      start = new Date();
+      //console.log(Math.floor(start.getTime()/1000) + secPerMonth*i);
+      retVector = wasm.getPositions(Math.floor(start.getTime()/1000));// + secPerMonth*i);
+      end = new Date();
+      elapsed = end.getTime() - start.getTime();
+      console.log(`CALC: ${elapsed}ms to calculate ${retVector.size()} positions`);
+      // for (let i = 0; i < retVector.size(); i++) {
+      //   let info = retVector.get(i);
+      //   console.log(`${info.get(0)}\t${info.get(1)}\t${info.get(2)}\t${info.get(3)}`);
+      // }
+      retVector.delete();
+    }
   }
 
   render().catch(console.error);
