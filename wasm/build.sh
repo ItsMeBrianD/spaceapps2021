154b3a09ab404dbe8408df0f901a54fdbda18068
Brian <contact@noimbrian.com>
2021-10-03 17:09:57 -0500

    O3 Build is Working

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Build the Wasm
 em++ src/main.cpp src/libastro/*.c -o ../svelte/src/lib/wasm/wasm.js \
-  -w -s WASM=1 -s TOTAL_MEMORY=1GB --bind -O1 \
+  -w -s WASM=1 -s TOTAL_MEMORY=1GB --bind -O3 \
   -std=c++11 --pre-js=pre.js --post-js=post.js \
   -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap","allocate"]'
 # Put the wasm file into the static directory so we can load it
 mv ../svelte/src/lib/wasm/wasm.wasm ../svelte/static/wasm.wasm;
 # Export Module so we can actually use it
 #sed "s/var Module = typeof Module !== 'undefined' ? Module : {};/export var Module = typeof Module !== 'undefined' ? Module : {};/g" ../svelte/src/lib/wasm/wasm.js -i;
-
+sed "s/module.exports=/export default/g" ../svelte/src/lib/wasm/wasm.js -i;
 echo "Built!";
