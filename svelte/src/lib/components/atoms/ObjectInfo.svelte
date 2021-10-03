2060e2625d54fdf8814947f1d41cf7ffe02c93af
Zachary Stence <zachstence@gmail.com>
2021-10-03 18:44:35 -0500

    link id to n2yo

--- svelte/src/lib/components/atoms/ObjectInfo.svelte
+++ svelte/src/lib/components/atoms/ObjectInfo.svelte
@@ -51,7 +51,9 @@
                 {#if $selectedObject}
                     <tr>
                         <th>Catalog Number</th>
-                        <td>{$selectedObject ? $selectedObject.id : ""}</td>
+                        <td>
+                            <a href={`https://www.n2yo.com/satellite/?s=${$selectedObject.id}#results`} target="_blank">{$selectedObject ? $selectedObject.id : ""}</a>
+                        </td>
                     </tr>
                 {/if}
                 {#if cat}
@@ -137,5 +139,9 @@
     }
     tbody:not(:first-child) {
         @apply border-t-2 border-secondary-500;
+    }
+
+    a:hover {
+        @apply cursor-pointer underline;
     }
 </style>
