3dcccd5ae60b74b43c6974c9143e450e07854960
Brian <contact@noimbrian.com>
2021-10-02 20:51:13 -0500

    stars and points

--- svelte/src/lib/components/atoms/Map.svelte
+++ svelte/src/lib/components/atoms/Map.svelte
@@ -1,29 +1,16 @@
 <script lang="ts">
-	import {onDestroy, onMount} from "svelte";
+	import {getContext, onDestroy, onMount} from "svelte";
+import type { TleStore } from "../../state/TleStore";
+import { ContextKeys } from "../../utils/constants";
+import { WorldWindModule } from "../../utils/WorldWindModule";
 	// import * as worldwind from "@nasaworldwind/worldwind";
 
 	let el: HTMLElement | undefined;
 	let canvas: HTMLCanvasElement | undefined;
-	onMount(async () => {
-		const WorldWind = await import("@nasaworldwind/worldwind");
-		console.log(WorldWind);
-		WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING)
+	const wasmStore: TleStore = getContext(ContextKeys.WasmStore);
 
-		const worldWindow = new WorldWind.WorldWindow(canvas);
-		const layers = [
-            // Imagery layers.
-            {layer: new WorldWind.BMNGLayer(), enabled: true},
-            {layer: new WorldWind.DigitalGlobeTiledImageLayer("Digital Globe", "digitalglobe.n6ngnadl", accessToken),
-                enabled: true},
-            {layer: new WorldWind.DigitalGlobeTiledImageLayer("Digital Globe with Roads", "digitalglobe.n6nhclo2", accessToken),
-                enabled: false},
-            // Add atmosphere layer on top of all base layers.
-            {layer: new WorldWind.AtmosphereLayer(), enabled: true},
-            // WorldWind UI layers.
-            {layer: new WorldWind.CompassLayer(), enabled: true},
-            {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
-            {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
-        ];
+	onMount(async () => {
+		WorldWindModule.init(canvas, wasmStore);
 	});
 	onDestroy(() => {
 	    
